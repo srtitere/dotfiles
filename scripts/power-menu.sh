6d80296f1608bdf7b01e957a6eeed032624fa0b7
Carlos Alonso Martín <sr.titere@gmail.com>
2021-06-03 13:22:58 +0200

    chage colors in rofi dark theme

--- scripts/power-menu.sh
+++ scripts/power-menu.sh
@@ -7,13 +7,12 @@
 poweroff_command="systemctl poweroff"
 reboot_command="systemctl reboot"
 logout_command="i3-msg exit"
-hibernate_command="systemctl hibernate"
+#hibernate_command="systemctl hibernate"
 suspend_command="systemctl suspend"
+lock_command="$HOME/scripts/fuzzy_lock.sh"
 
-# you can customise the rofi command all you want ...
-rofi_command="rofi -width 10 -hide-scrollbar -bg #586e75 -opacity 100 -padding 5"
+rofi_command="rofi -lines 5"
 
-options=$'poweroff\nreboot\nlogout\nhibernate\nsuspend' 
+options=$'poweroff\nreboot\nlogout\nsuspend\nlock'
 
-# ... because the essential options (-dmenu and -p) are added here
-eval \$"$(echo "$options" | $rofi_command -dmenu -p "")_command"
+eval "\$$(echo "$options" | $rofi_command -dmenu -p "")_command"
